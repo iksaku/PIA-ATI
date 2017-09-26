cc2bc1738cd128d5b62eceaaedec4c3f6db8e891
Jorge González <iksaku@me.com>
2017-09-26 10:59:30 -0500

    Fixed social button grouping

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ $(function() {
     localStorage.setItem("visits", visitor);
     
     if (visitor === 999999) {
-        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning btn-lg\">Si me ves, mancha te debe una asesoría gratuita.</button>"));
+        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning\">Si me ves, mancha te debe una asesoría gratuita.</button>"));
     }
 });
 
