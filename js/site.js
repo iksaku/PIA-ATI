c44ac68f07b7a50fe100efbab9926cbb6b140118
Jorge González <iksaku@me.com>
2017-09-25 22:59:13 -0500

    Primer Version

---
+++ js/site.js
@@ -0,0 +1,21 @@
+var visitor = getVisitorNumber();
+
+$(function() {
+    $("#visitCount").text(numberFormatting(++visitor));
+    localStorage.setItem("visits", visitor);
+    
+    if (visitor === 999999) {
+        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning btn-lg\">Si me ves, mancha te debe una asesoría gratuita.</button>"));
+    }
+});
+
+function numberFormatting(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getVisitorNumber() {
+    if (!localStorage.getItem("visits")) {
+        localStorage.setItem("visits", 999995);
+    }
+    return localStorage.getItem("visits")
+}
