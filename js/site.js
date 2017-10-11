1bb9264b7c07f469477dfffac8495888c1acc7f8
Jorge González <iksaku@me.com>
2017-10-10 21:18:50 -0500

    Trying to play with video tags in JS

--- js/site.js
+++ js/site.js
@@ -1,12 +1,8 @@
 var visitor = getVisitorNumber();
 
 $(function() {
-    $("#visitCount").text(numberFormatting(++visitor));
-    localStorage.setItem("visits", visitor);
-    
-    if (visitor === 999999) {
-        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning\">Si me ves, <strong>Mancha</strong> te debe una asesoría gratuita.</button>"));
-    }
+    updateVisitsCount();
+    //videoHelper();
 });
 
 function numberFormatting(number) {
@@ -18,4 +14,19 @@ function getVisitorNumber() {
         localStorage.setItem("visits", 999995);
     }
     return localStorage.getItem("visits")
+}
+
+function updateVisitsCount() {
+    $("#visitCount").text(numberFormatting(++visitor));
+    localStorage.setItem("visits", visitor);
+    
+    if (visitor === 999999) {
+        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning\">Si me ves, <strong>Mancha</strong> te debe una asesoría gratuita.</button>"));
+    }
+}
+
+function videoHelper() {
+    $("video").each(function(index, element) {
+        console.log($(this).text);
+    });
 }
