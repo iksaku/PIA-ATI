0aa99bddecbd4cd5efa096ee0ed35bcc66f6268f
Jorge González <iksaku@me.com>
2017-10-14 00:28:58 -0500

    Animated Font Awesome...
    - Better EasterEgg

--- js/site.js
+++ js/site.js
@@ -1,5 +1,3 @@
-var visitor = getVisitorNumber();
-
 $(function() {
     updateVisitsCount();
     videoHelper();
@@ -17,16 +15,17 @@ function getVisitorNumber() {
 }
 
 function updateVisitsCount() {
+    var visitor = getVisitorNumber();
     $("#visitCount").text(numberFormatting(++visitor));
     localStorage.setItem("visits", visitor);
     
     if (visitor === 999999) {
-        $("#easterEgg1").append($("<button type=\"button\" class=\"btn btn-warning\">Si me ves, <strong>Mancha</strong> te debe una asesoría gratuita.</button>"));
+        $("#easterEgg1").append($('<div class="panel panel-warning text-center"><div class="panel-heading"><h1 class="panel-title"><span class="fa fa-bell fa-3x faa-ring faa-slow animated"></span><span class="fa fa-gift fa-3x faa-shake animated"></span><span class="fa fa-bell fa-3x faa-ring faa-slow animated"></span></h1></div><div class="panel-body">Si me ves, <strong>Mancha</strong> te debe una asesoria gratuita...</div></div>'));
     }
 }
 
 function videoHelper() {
     $("video").each(function(index, element) {
-        $(this).append($('<div class="panel panel-danger vertical-center"><div class="panel-heading"><i class="fa fa-warning" aria-hidden="true"></i> <strong>Huston... Tenemos un problema</strong> <i class="fa fa-warning" aria-hidden="true"></i></div><div class="panel-body">Tu navegador no soporta la reproducción de videos, sin embargo, puedes descargar el video <a href="' + $(this).attr("src") + '" class="btn btn-xs btn-primary">aquí</a> y reproducirlo con tu aplicación favorita. <i class="fa fa-youtube-play"></i></div></div>'));
+        $(this).append($('<div class="panel panel-danger vertical-center text-center"><div class="panel-heading"><h3 class="panel-title"><span class="fa fa-warning" aria-hidden="true"></span> Huston... Tenemos un problema <span class="fa fa-warning" aria-hidden="true"></span></div><div class="panel-body">Tu navegador no soporta la reproducción de videos, sin embargo, puedes descargar el video <a href="' + $(this).attr("src") + '" class="btn btn-xs btn-primary">aquí</a> y reproducirlo con tu aplicación favorita. <span class="fa fa-youtube-play"></span></h3></div></div>'));
     });
 }
