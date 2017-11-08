2f2c1837efad805d62feab5d11de9b57976a62cb
Jorge González <iksaku@me.com>
2017-11-08 16:29:57 -0600

    Users db :white_check_mark: + Automated "About Us" page Some progress on commenting system

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 $(function() {
+    databaseHelper();
     updateVisitsCount();
     videoHelper();
 });
@@ -7,25 +8,90 @@ function numberFormatting(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function getVisitorNumber() {
-    if (!localStorage.getItem("visits")) {
-        localStorage.setItem("visits", 999995);
+careers = {
+    "its": "Ingeniería en Tecnologías de Software",
+    "imtc": "Ingeniería en Mecatrónica"
+};
+
+databaseModel = {
+    "visits": 999996,
+    "users": [
+        {
+            "username": "j.gonzalez",
+            "name": "Jorge Alejandro González Guerra",
+            "career": "its",
+            "photo": "images/Jorge.jpeg"
+        },
+        {
+            "username": "g.caldera",
+            "name": "Gerardo Jahir Caldera Perez",
+            "career": "imtc",
+            "photo": "images/Jahir.jpg"
+        },
+        {
+            "username": "l.orrante",
+            "name": "Luis Fernando Orrante Vales",
+            "career": "imtc",
+            "photo": "images/Luis.jpg"
+        },
+        {
+            "username": "e.ferrer",
+            "name": "Eduardo Ferrer Treviño",
+            "career": "its",
+            "photo": "images/Eduardo.jpg"
+        },
+        {
+            "username": "h.solis",
+            "name": "Habib Yosef Natanael Solis Flores",
+            "career": "its",
+            "photo": "images/Habib.jpg"
+        }
+    ],
+    "comments": {
+        "post-001": [
+            {
+                "author": "j.gonzalez",
+                "timestamp": "1510172639616",
+                "content": "Tttt"
+            }        ]
     }
-    return localStorage.getItem("visits")
+};
+
+var database = {
+    set: function(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    get: function(key) {
+        var item = localStorage.getItem(key);
+        return !item ? false : JSON.parse(item);
+    }
+};
+
+function databaseHelper() {
+    Object.keys(databaseModel).forEach(function(k) {
+        if (!database.get(k)) {
+            database.set(k, databaseModel[k]);
+        }
+    })
 }
 
 function updateVisitsCount() {
-    var visitor = getVisitorNumber();
+    var visitor = database.get("visits");
     $("#visitCount").text(numberFormatting(++visitor));
-    localStorage.setItem("visits", visitor);
-    
-    if (visitor === 999999) {
-        $("#easterEgg1").append($('<div class="panel panel-warning text-center"><div class="panel-heading"><h1 class="panel-title"><span class="fa fa-bell fa-3x faa-ring faa-slow animated"></span><span class="fa fa-gift fa-3x faa-shake animated"></span><span class="fa fa-bell fa-3x faa-ring faa-slow animated"></span></h1></div><div class="panel-body">Si me ves, <strong>Mancha</strong> te debe una asesoria gratuita...</div></div>'));
-    }
+    database.set("visits", visitor);
 }
 
 function videoHelper() {
-    $("video").each(function(index, element) {
-        $(this).append($('<div class="panel panel-danger vertical-center text-center"><div class="panel-heading"><h3 class="panel-title"><span class="fa fa-warning" aria-hidden="true"></span> Huston... Tenemos un problema <span class="fa fa-warning" aria-hidden="true"></span></div><div class="panel-body">Tu navegador no soporta la reproducción de videos, sin embargo, puedes descargar el video <a href="' + $(this).attr("src") + '" class="btn btn-xs btn-primary">aquí</a> y reproducirlo con tu aplicación favorita. <span class="fa fa-youtube-play"></span></h3></div></div>'));
+    $("video").each(function() {
+        var src = $(this).attr("src");
+        $(this).append(
+            $("<div></div>").addClass("panel panel-danger vertical-center text-center").append(
+                $("<div></div>").addClass("panel-heading").append(
+                    $("<h3></h3>").addClass("panel-title").text("<span class=\"fa fa-warning\" aria-hidden=\"true\"></span> Huston... Tenemos un problema <span class=\"fa fa-warning\" aria-hidden=\"true\"></span>")
+                ),
+                $("<div></div>").addClass("panel-body").text("Tu navegador no soporta la reproducción de videos, sin embargo, puedes descargar el video <a href=\"" + src + "\" class=\"btn btn-xs btn-primary\">aquí</a> y reproducirlo con tu aplicación favorita. <span class=\"fa fa-youtube-play\"></span>")
+            )
+        )
     });
 }
